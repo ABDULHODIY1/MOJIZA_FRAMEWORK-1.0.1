76f7ec10019b3a1c7dc7455d5d7811f50c198cf0
bdulhodiy <muhiddinovabdulhodiy777@gmail.com>
2025-07-30 22:42:13 +0500

    Fix: PYPI ga yukladim

--- setup.py
+++ setup.py
@@ -2,29 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="mojiza",
-    version="0.0.1",
-    description="A lightweight Python HTML rendering engine",
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    author="Abdulhodiy Muhiddinov",
-    author_email="muhiddinovabdulhodiy2@gmail.com",
-    url="https://github.com/ABDULHODIY1/MOJIZA_FRAMEWORK-1.0.1",
+    version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        "Jinja2>=3.1.4",
-        "loguru>=0.7.3",
-        "watchdog>=6.0.0",
-        "aiohttp>=3.11.10",
-        "qrcode>=8.0",
-    ],
+    install_requires=[],
+    license="GPL-3.0-only",  # Faqat bu yerda ko'rsatiladi
+    description="MOJIZA - A minimal web framework",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Framework :: Mojiza",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        # License classifier NI o‘chirib tashlang!
     ],
-    python_requires='>=3.7',
-    license="MIT",
+    python_requires='>=3.6',
 )
