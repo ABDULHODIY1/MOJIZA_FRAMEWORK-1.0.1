70f16c83e085d25304c55dc2d83e4de3366cd1fe
bdulhodiy <muhiddinovabdulhodiy777@gmail.com>
2025-08-02 14:48:30 +0500

    FIX: change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mojiza",
-    version="0.1.3",
+    version="0.1.3.b1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
